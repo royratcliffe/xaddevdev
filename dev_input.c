0b8b4d828cca0b1748987c3422dc1ef3d41bdf86
Roy Ratcliffe <roy@ratcliffe.me>
2026-04-25 11:19:17 +0100

    Change log level for input event logging to debug
    Adjust the logging level for input events from info to debug
    to reduce verbosity in the logs. This change helps in
    focusing on critical information while debugging input
    device interactions.

--- dev_input.c
+++ dev_input.c
@@ -105,7 +105,7 @@ CAUSES(epoll_event, dev_input_epoll_event) {
     pr_warn("Failed to read input event for device %s\n", name);
     return;
   }
-  pr_info("Input event: device=%s type=%u code=%u value=%d\n", name, input_event.type, input_event.code, input_event.value);
+  pr_debug("Input event: device=%s type=%u code=%u value=%d\n", name, input_event.type, input_event.code, input_event.value);
   OCCURS(input_event, &input_event, name);
 }
 
